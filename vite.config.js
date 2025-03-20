2380ce46bf3535880b18a3d2d49ab3d9a46d29ad
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2025-03-20 08:52:30 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/REACT_Form_Handelling_Practise/',
   plugins: [tailwindcss(), react()],
 });
